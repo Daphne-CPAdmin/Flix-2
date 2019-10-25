4139b202ddb2735a93fb2a5b817cd879cfafafa3
Dan <tonthatd@gmail.com>
2019-10-25 11:50:25 -0700

    adding assignment 2

---
+++ Flix/MovieGridCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flix
+//
+//  Created by maggie on 10/24/19.
+//  Copyright © 2019 dan. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
